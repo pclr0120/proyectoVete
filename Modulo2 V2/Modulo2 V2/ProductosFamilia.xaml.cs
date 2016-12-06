00fbd2ecb284a942e9cc1498b4bcbb3e34034452
cesar-soto96@hotmail.com <cesar-soto96@hotmail.com>
2016-12-06 15:04:03 -0700

    INGRESANDO MODULO 2 SI SALE

---
+++ Modulo2 V2/Modulo2 V2/ProductosFamilia.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Modulo2_V2
+{
+    /// <summary>
+    /// Lógica de interacción para ProductosFamilia.xaml
+    /// </summary>
+    public partial class ProductosFamilia : Window
+    {
+        public ProductosFamilia()
+        {
+            InitializeComponent();
+        }
+
+        private void CLAVEFAM_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
