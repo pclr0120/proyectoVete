206d084db4b3296b719a4dd4e52767e1aea7e560
pclrpclr <pclrpclr@DESKTOP-LM0PHOC>
2016-12-05 22:48:12 -0700

    Modi

--- ModuloPricipal/ModuloPricipal/PuntoVenta.xaml.cs
+++ ModuloPricipal/ModuloPricipal/PuntoVenta.xaml.cs
@@ -52,7 +52,8 @@ namespace ModuloPricipal
         string valor;
         int c = 0;
         int venta;
-        int dd, cc, iva;
+        double dd, iva;
+        int cc;
         int registroProducto = 1;
 
         public void reloj()
@@ -195,6 +196,18 @@ namespace ModuloPricipal
         {
             DialogoEliminarProducto win = new DialogoEliminarProducto();
             win.ShowDialog();
+            int valor = win.valor-1;
+            //ACtualizar LAS label
+            //dd -= Convert.ToInt32(((DataRowView)dataGrid.Items[win.valor]).Row[3]);
+            dd -= lista[valor].Precio;
+            //iva -= Convert.ToInt32(((DataRowView)dataGrid.Items[win.valor]).Row[2]);
+            iva -= lista[valor].Iva;
+            cc -= 1;
+            lbliva.Content = iva.ToString();
+            lblsubtol.Content = dd.ToString();
+
+            lbltotal.Content = (dd + iva).ToString();
+            //label
             lista.Remove(lista.FirstOrDefault(c => c.Registro1 == win.valor));
             int ccc = 1;
             for (int i = 0; i < lista.Count; i++)
@@ -235,12 +248,12 @@ namespace ModuloPricipal
             }
         }
 
-        static List<producto> Leer(MySqlCommand cmd,int c)
+      public  static List<producto> Leer(MySqlCommand cmd,int c)
         {
 
             using (MySqlDataReader dr = cmd.ExecuteReader())
             {
-                 
+                
                 int ID = dr.GetOrdinal("idProductos");
                 int IDCentroOperacion = dr.GetOrdinal("nombre");
                 int IDOrganizacion = dr.GetOrdinal("precio");
@@ -257,6 +270,7 @@ namespace ModuloPricipal
                     t.Precio = values[IDOrganizacion] == DBNull.Value ? 0 : Convert.ToDouble(values[IDOrganizacion]);
                     t.Iva =values[Descripcion] == DBNull.Value ? 0 : Convert.ToDouble(values[Descripcion]);
                     lista.Add(t);
+                
                     t = new producto();
 
                   
@@ -292,14 +306,13 @@ namespace ModuloPricipal
     }
         producto prod = new producto();
        
-        private void Addventa()
+        private void Addventa( )
 
     {
         try
         {
 
-            
-               
+    
                 valor = cb_produc.SelectedValue.ToString();
                 int id = Convert.ToInt32(valor);
                 Consultar(id, conexion,registroProducto);
@@ -307,38 +320,37 @@ namespace ModuloPricipal
                 //prod.Registro1 = registroProducto;
 
                 //lista.Add(prod);
-
                 this.dataGrid.ItemsSource = lista;
                 this.dataGrid.Items.Refresh();
+               
+                sumar();
                 registroProducto += 1;
-
-
-
-
 
             }
         catch (Exception e) { MessageBox.Show("Consulte a su Administrador:" + e, "Mensaje de Error"); }
     }
 
-    //private void sumar()
-    //{
-    //    try
-    //    {
-    //        //consultarPro();
-    //        iva += Convert.ToInt32(((DataRowView)dataGrid.Items[cc]).Row[6]);
-    //        dd += Convert.ToInt32(((DataRowView)dataGrid.Items[cc]).Row[3]);
-    //        cc += 1;
-    //        lbliva.Content = iva.ToString();
-    //        lblsubtol.Content = dd.ToString();
-    //        lbltotal.Content = dd + iva;
+        public void sumar()
+        {
+            try
+            {
+                //consultarPro();
+               
+                iva += lista[registroProducto-1].Iva;
+                dd += lista[registroProducto-1].Precio;
+                cc += 1;
+                lbliva.Content = iva.ToString();
+                lblsubtol.Content = dd.ToString();
+                lbltotal.Content = dd + iva;
 
-    //    }
-    //    catch (Exception e) { MessageBox.Show("Consulte a su Administrador:" + e, "Mensaje de Error"); }
-    //}
+            }
+            catch (Exception e) { MessageBox.Show("Consulte a su Administrador:" + e, "Mensaje de Error"); }
+        }
 
-    private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
     {
         Addventa();
+           
         //sumar();
     }
 
@@ -379,37 +391,31 @@ namespace ModuloPricipal
         }
 
     }
-    public void EliminarProducto(int NumeroRegistro)
-    {
-        //int valor;
+    //public void EliminarProducto(int NumeroRegistro)
+    //{
+    //    //int valor;
 
-        //dataGrid.Items.Remove(DeleteIndex);
-        try
-        {
-            dd -= Convert.ToInt32(((DataRowView)dataGrid.Items[NumeroRegistro]).Row[3]);
+    //    //dataGrid.Items.Remove(DeleteIndex);
+    //    try
+    //    {
+    //        dd -= Convert.ToInt32(((DataRowView)dataGrid.Items[NumeroRegistro]).Row[3]);
 
-            iva -= Convert.ToInt32(((DataRowView)dataGrid.Items[NumeroRegistro]).Row[6]);
-            cc -= 1;
-            lbliva.Content = iva.ToString();
-            lblsubtol.Content = dd.ToString();
+    //        iva -= Convert.ToInt32(((DataRowView)dataGrid.Items[NumeroRegistro]).Row[6]);
+    //        cc -= 1;
+    //        lbliva.Content = iva.ToString();
+    //        lblsubtol.Content = dd.ToString();
 
-            lbltotal.Content = (dd + iva).ToString();
-            consultap.Tables["productos"].Rows.RemoveAt(NumeroRegistro);
+    //        lbltotal.Content = (dd + iva).ToString();
 
-
-
-
-            dataGrid.ItemsSource = consultap.Tables["productos"].DefaultView;
-            NumeroRegistro = -1;
-            MessageBox.Show("Eliminado", "Mensaje");
-        }
-        catch (Exception)
-        {
-            MessageBox.Show("seleccione un producto para Eliminar", "Mensaje");
-        }
+    //        MessageBox.Show("Eliminado", "Mensaje");
+    //    }
+    //    catch (Exception)
+    //    {
+    //        MessageBox.Show("seleccione un producto para Eliminar", "Mensaje");
+    //    }
 
 
-    }
+    //}
 
     private void dataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
     {
@@ -493,14 +499,17 @@ namespace ModuloPricipal
                     cmd.Transaction = tranOperaciones;
                     try
                     {
-                        _id = Convert.ToInt32(((DataRowView)dataGrid.Items[i]).Row["idProductos"].ToString());
-                        _iva = Convert.ToDouble(((DataRowView)dataGrid.Items[i]).Row["iva"].ToString());
+                            _id = lista[i].Id;
+                            _iva = lista[i].Iva;
+                            _precio = lista[i].Precio;
+                        //_id = Convert.ToInt32(((DataRowView)dataGrid.Items[i]).Row["idProductos"].ToString());
+                        //_iva = Convert.ToDouble(((DataRowView)dataGrid.Items[i]).Row["Iva"].ToString());
 
-                        _precio = Convert.ToDouble(((DataRowView)dataGrid.Items[i]).Row["precio"].ToString());
+                            //_precio = Convert.ToDouble(((DataRowView)dataGrid.Items[i]).Row["Precio"].ToString());
 
 
 
-                        cmd.CommandText = "RegistrarVenta";
+                            cmd.CommandText = "RegistrarVenta";
                         cmd.Parameters.Clear();
                         cmd.Parameters.Add(new MySqlParameter("_venta", venta));
                         cmd.Parameters.Add(new MySqlParameter("_id", _id));
