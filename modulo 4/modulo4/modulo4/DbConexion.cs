11ff6d7dc7ffb25c9fcb2d42227d09836d2d23ac
zicaxzx@gmail.com <zicaxzx@gmail.com>
2016-12-14 13:26:37 -0700

    ahora si agregue las pruebas unitarias y ademas se agregan estandares

---
+++ modulo 4/modulo4/modulo4/DbConexion.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeterinariaClientes
+{
+    public class DbConexion
+    {
+        String StrConn;
+        MySqlConnection Conn;
+        public DbConexion(String Server,String User,String Password,String Database ) {
+            StrConn = "server=" + Server + "; database = " + Database + "; Uid=" + User + "; pwd=" + Password + ";";
+        }
+        public bool Abrir()
+        {
+            try
+            {
+                Conn = new MySqlConnection(StrConn);
+                Conn.Open();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public MySqlConnection Get()
+        {
+            return Conn;
+        }
+
+        public bool Cerrar()
+        {
+            try
+            {
+                Conn.Close();
+                return true;
+            }
+            catch { return false; }
+        }
+    }
+}
---
+++ modulo 4/modulo4/modulo4Tests/DbConexionTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VeterinariaClientes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeterinariaClientes.Tests
+{
+    [TestClass()]
+    public class DbConexionTests
+    {
+        [TestMethod()]
+        public void DbConexionTest()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void AbrirTest()
+        {
+            DbConexion sesultado = new DbConexion("localhost","root","toor","bdvt");
+            bool esperado = true;
+            bool resultado = sesultado.Abrir();
+            
+            Assert.AreEqual(esperado, resultado);
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CerrarTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
